8b72c544c610852dc1cfdec6ce7e3685f8ae6c92
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-02 22:03:12 +0100

    F** Matrix3 constructor added

--- Geometrica/DataStructures/Matrix3.cs
+++ Geometrica/DataStructures/Matrix3.cs
@@ -8,5 +8,23 @@ namespace Geometrica.DataStructures
 {
     public class Matrix3
     {
+        private double[,] matrix;
+
+        public Matrix3()
+        {
+            matrix = new double[3, 3];
+            for(int i = 0; i < 3; i++)
+            {
+                for(int j = 0; j < 3; j++)
+                {
+                    matrix[i, j] = 0;
+                }
+            }
+        }
+
+        public override string ToString()
+        {
+            return "";
+        }
     }
 }
