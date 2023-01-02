17bbe000554880ed715504b8d7e1f0f771657d34
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-02 22:18:42 +0100

    r Code prettified

--- Geometrica/DataStructures/Matrix3.cs
+++ Geometrica/DataStructures/Matrix3.cs
@@ -1,36 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Geometrica.DataStructures;
 
-namespace Geometrica.DataStructures
+public class Matrix3
 {
-    public class Matrix3
-    {
-        private double[,] matrix;
+    private double[,] matrix;
 
-        public Matrix3()
+    public Matrix3()
+    {
+        matrix = new double[3, 3];
+        for(int i = 0; i < 3; i++)
         {
-            matrix = new double[3, 3];
-            for(int i = 0; i < 3; i++)
+            for(int j = 0; j < 3; j++)
             {
-                for(int j = 0; j < 3; j++)
-                {
-                    matrix[i, j] = 0;
-                }
+                matrix[i, j] = 0;
             }
         }
+    }
 
-        public double this[int row, int column]
-        {
-            get => matrix[row, column];
-            set => matrix[row, column] = value;
-        }
+    public double this[int row, int column]
+    {
+        get => matrix[row, column];
+        set => matrix[row, column] = value;
+    }
 
-        public override string ToString()
-        {
-            return $"[{matrix[0, 0]};{matrix[0, 1]};{matrix[0, 2]}][{matrix[1, 0]};{matrix[1, 1]};{matrix[1, 2]}][{matrix[2, 0]};{matrix[2, 1]};{matrix[2, 2]}]";
-        }
+    public override string ToString()
+    {
+        return $"[{matrix[0, 0]};{matrix[0, 1]};{matrix[0, 2]}][{matrix[1, 0]};{matrix[1, 1]};{matrix[1, 2]}][{matrix[2, 0]};{matrix[2, 1]};{matrix[2, 2]}]";
     }
 }
