b24847242fe908230de0d33d514e33196a1b2168
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-02 22:44:20 +0100

    r Code prettified

--- GeometricaTests/MatrixTests.cs
+++ GeometricaTests/MatrixTests.cs
@@ -1,36 +1,34 @@
 using Geometrica.DataStructures;
-using System.Numerics;
 
-namespace GeometricaTests
+namespace GeometricaTests;
+
+[TestClass]
+public class MatrixTests
 {
-    [TestClass]
-    public class MatrixTests
+    private readonly double delta = 1e-3;
+
+    [TestMethod]
+    public void Matrix3_IsZero_ByDefault()
     {
-        private readonly double delta = 1e-3;
+        var m = new Matrix3();
 
-        [TestMethod]
-        public void Matrix3_IsZero_ByDefault()
-        {
-            var m = new Matrix3();
+        Assert.AreEqual("[0;0;0][0;0;0][0;0;0]", m.ToString());
+    }
 
-            Assert.AreEqual("[0;0;0][0;0;0][0;0;0]", m.ToString());
-        }
+    [TestMethod]
+    public void Matrix3_Items_CanBeAccessedWithIndexer()
+    {
+        var m = new Matrix3();
 
-        [TestMethod]
-        public void Matrix3_Items_CanBeAccessedWithIndexer()
-        {
-            var m = new Matrix3();
+        m[0, 0] = 5;
+        Assert.AreEqual(5, m[0, 0], delta);
+    }
 
-            m[0, 0] = 5;
-            Assert.AreEqual(5, m[0, 0], delta);
-        }
+    [TestMethod]
+    public void Matrix3_UnitMatrix_InitializedCorrectly()
+    {
+        var m = Matrix3.Unit;
 
-        [TestMethod]
-        public void Matrix3_UnitMatrix_InitializedCorrectly()
-        {
-            var m = Matrix3.Unit;
-
-            Assert.AreEqual("[1;0;0][0;1;0][0;0;1]", m.ToString());
-        }
+        Assert.AreEqual("[1;0;0][0;1;0][0;0;1]", m.ToString());
     }
 }
