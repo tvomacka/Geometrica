88102da17fed0f043d1366f10510e11ea4695d9f
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-03 21:56:32 +0100

    R!! Minor refactoring

--- Geometrica/Primitives/Triangle.cs
+++ Geometrica/Primitives/Triangle.cs
@@ -1,12 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿namespace Geometrica.Primitives;
 
-namespace Geometrica.Primitives
+public struct Triangle
 {
-    internal class Triangle
-    {
-    }
 }
