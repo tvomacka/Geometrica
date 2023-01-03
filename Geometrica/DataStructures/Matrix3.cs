25eb2276bcfe467e1900ecc88bc0c4bf88709b2d
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-03 21:21:56 +0100

    F** Matrix transpose

--- Geometrica/DataStructures/Matrix3.cs
+++ Geometrica/DataStructures/Matrix3.cs
@@ -55,4 +55,9 @@ public struct Matrix3
     {
         return $"[{matrix[0, 0]};{matrix[0, 1]};{matrix[0, 2]}][{matrix[1, 0]};{matrix[1, 1]};{matrix[1, 2]}][{matrix[2, 0]};{matrix[2, 1]};{matrix[2, 2]}]";
     }
+
+    public object Transpose()
+    {
+        throw new NotImplementedException();
+    }
 }
