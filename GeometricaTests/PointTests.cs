d264a5e9367260f9e7be625d9707ef9e714b9dcd
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-03 21:43:41 +0100

    t** Point initialization test

--- GeometricaTests/PointTests.cs
+++ GeometricaTests/PointTests.cs
@@ -1,6 +1,15 @@
-﻿namespace GeometricaTests;
+﻿using Geometrica.Primitives;
+
+namespace GeometricaTests;
 
 [TestClass]
 public class PointTests
 {
+    [TestMethod]
+    public void Point_Default_IsZero()
+    {
+        var p = new Point2();
+
+        Assert.AreEqual("", p.ToString());
+    }
 }
