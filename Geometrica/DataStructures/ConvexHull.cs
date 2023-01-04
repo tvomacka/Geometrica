27e7f51bbd52b9d4e0e75d82b6de4e369a0454c7
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-04 22:50:54 +0100

    F** Point can be added to convex hull

--- Geometrica/DataStructures/ConvexHull.cs
+++ Geometrica/DataStructures/ConvexHull.cs
@@ -1,5 +1,13 @@
-﻿namespace Geometrica.DataStructures;
+﻿using Geometrica.Primitives;
+
+namespace Geometrica.DataStructures;
 
 public class ConvexHull
 {
+    public object Hull { get; set; }
+
+    public void Add(Point2 point2)
+    {
+
+    }
 }
--- GeometricaTests/ConvexHullTests.cs
+++ GeometricaTests/ConvexHullTests.cs
@@ -1,6 +1,18 @@
-﻿namespace GeometricaTests;
+﻿using Geometrica.DataStructures;
+using Geometrica.Primitives;
+
+namespace GeometricaTests;
 
 [TestClass]
 public class ConvexHullTests
 {
+    [TestMethod]
+    public void ConvexHull_OfTwoPoints_DoesNotExist()
+    {
+        var ch = new ConvexHull();
+        ch.Add(new Point2());
+        ch.Add(new Point2(10, 10));
+
+        Assert.IsNull(ch.Hull);
+    }
 }
