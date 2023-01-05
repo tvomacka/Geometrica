cc1bda3a99d4b72bfbcf7696ada03f473334b875
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-05 20:29:50 +0100

    R** Convex Hull update improved

--- Geometrica/DataStructures/ConvexHull.cs
+++ Geometrica/DataStructures/ConvexHull.cs
@@ -5,11 +5,25 @@ namespace Geometrica.DataStructures;
 public class ConvexHull
 {
     private readonly List<Point2> points = new();
-    public readonly List<Point2> Hull = new();
+    public readonly List<int> Hull = new();
 
     public void Add(Point2 point)
     {
         points.Add(point);
+        UpdateConvexHull();
+    }
+
+    private void UpdateConvexHull()
+    {
+        if (points.Count < 3) 
+        { 
+            Hull.Clear();
+            return;
+        }
+        else if (points.Count == 3)
+        {
+            Hull.AddRange(new List<int>(){ 0, 1, 2});
+        }
     }
 
     public override string ToString()
