6a79721968912a4638300d9be70034004a985d2e
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-10 22:51:25 +0100

    R!! Hull refactoring

--- Geometrica/DataStructures/ConvexHull.cs
+++ Geometrica/DataStructures/ConvexHull.cs
@@ -5,7 +5,11 @@ namespace Geometrica.DataStructures;
 public class ConvexHull
 {
     private readonly List<Point2> points = new();
-    public readonly List<int> Hull = new();
+    public readonly List<Point2> Hull = new();
+
+    public ConvexHull()
+    {
+    }
 
     public ConvexHull(List<Point2> pts)
     {
@@ -13,10 +17,10 @@ public class ConvexHull
         Hull = CreateConvexHull(pts);
     }
 
-    public List<int> CreateConvexHull(List<Point2> pts)
+    public List<Point2> CreateConvexHull(List<Point2> pts)
     {
         //divide and conquer
-        if(6 < pts.Count())
+        if(pts.Count() > 6)
         {
             var half = pts.Count / 2;
             return JoinHulls(CreateConvexHull(pts.GetRange(0, half)), CreateConvexHull(pts.GetRange(half, pts.Count() - half)));
@@ -27,12 +31,33 @@ public class ConvexHull
         }
     }
 
-    public List<int> CreateSimpleHull(List<Point2> pts)
+    public List<Point2> CreateSimpleHull(List<Point2> pts)
     {
-        throw new NotImplementedException();
+        if(pts == null)
+        {
+            throw new ArgumentNullException($"The argument {nameof(pts)} must not be null. Please provide a list points.");
+        }
+        if(3 > pts.Count() || pts.Count > 6)
+        {
+            throw new ArgumentException($"This method must only be used for 3 to 5 points. You provided a list of {pts.Count()} points.");
+        }
+        
+        if(pts.Count() == 3)
+        {
+            if (Point2.Orientation(pts[0], pts[1], pts[2]) < 0)
+            {
+                return new List<Point2>() { pts[0], pts[1], pts[2] };
+            }
+            else
+            {
+                return new List<Point2>() { pts[0], pts[2], pts[1] };
+            }
+        }
+
+        return null;
     }
 
-    public List<int> JoinHulls(List<int> list1, List<int> list2)
+    public List<Point2> JoinHulls(List<Point2> list1, List<Point2> list2)
     {
         throw new NotImplementedException();
     }
@@ -52,30 +77,19 @@ public class ConvexHull
         }
         else if (points.Count == 3)
         {
-            Hull.AddRange(new List<int>(){ 0, 1, 2});
+            Hull.AddRange(points);
         }
         else
         {
-            if (!points.Last().Inside(GetHullPoints()))
+            if (!points.Last().Inside(Hull))
             {
                 //reconstruct the hull
             }
         }
     }
 
-    private List<Point2> GetHullPoints()
-    {
-        var hullPoints = new List<Point2>();
-        foreach(var i in Hull)
-        {
-            hullPoints.Add(points[i]);
-        }
-
-        return hullPoints;
-    }
-
     public override string ToString()
     {
-        return string.Join(" ", GetHullPoints());
+        return string.Join(" ", Hull);
     }
 }
