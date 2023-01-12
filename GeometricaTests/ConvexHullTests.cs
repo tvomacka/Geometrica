2ed0a0712c014fa4e702df32a223689da381e1ea
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-12 20:46:03 +0100

    t Test added for CH4 ABDC CCW case

--- GeometricaTests/ConvexHullTests.cs
+++ GeometricaTests/ConvexHullTests.cs
@@ -134,6 +134,19 @@ public class ConvexHullTests
         Assert.AreEqual("[0; 0] [1; 0] [1; 1] [0; 1]", points);
     }
 
+    [TestMethod]
+    public void ConvexHull4_ABDC_CCW_ReturnsABDC()
+    {
+        var a = new Point2(0, 0);
+        var b = new Point2(1, 0);
+        var d = new Point2(1, 1);
+        var c = new Point2(0, 1);
+
+        var ch = ConvexHull.ConvexHull4(a, b, c, d);
+        var points = String.Join(" ", ch);
+        Assert.AreEqual("[0; 0] [1; 0] [1; 1] [0; 1]", points);
+    }
+
     public void Sample()
     {
         // begin-snippet: CreateConvexHull
