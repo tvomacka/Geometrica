b078064904b01847066a68baf472c1528439ef86
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-01-12 20:51:52 +0100

    t Test added for CH4 ABD CCW

--- GeometricaTests/ConvexHullTests.cs
+++ GeometricaTests/ConvexHullTests.cs
@@ -160,6 +160,19 @@ public class ConvexHullTests
         Assert.AreEqual("[0; 0] [1; 0] [1; 1] [0; 1]", points);
     }
 
+    [TestMethod]
+    public void ConvexHull4_ABD_CCW_ReturnsABD()
+    {
+        var a = new Point2(0, 0);
+        var b = new Point2(1, 0);
+        var d = new Point2(0, 1);
+        var c = new Point2(0.1, 0.1);
+
+        var ch = ConvexHull.ConvexHull4(a, b, c, d);
+        var points = String.Join(" ", ch);
+        Assert.AreEqual("[0; 0] [1; 0] [0; 1]", points);
+    }
+
     public void Sample()
     {
         // begin-snippet: CreateConvexHull
