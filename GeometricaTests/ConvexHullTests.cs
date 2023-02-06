858bf93ac4001edd6acae03cd373269fca877dfb
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-06 21:59:23 +0100

    t GrahamSearch test added

--- GeometricaTests/ConvexHullTests.cs
+++ GeometricaTests/ConvexHullTests.cs
@@ -273,11 +273,25 @@ public class ConvexHullTests
         var d = new Point2(1, 3);
         var e = new Point2(0, 2);
 
-        var ch = ConvexHull.BruteForce(new Point2[] { a, b, c, d, e });
+        var ch = ConvexHull.BruteForce(new[] { a, b, c, d, e });
         // end-snippet
 
         var points = string.Join(" ", ch);
         Assert.AreEqual("[0; 0] [2; 0] [2; 3] [1; 3] [0; 2]", points);
+    }
+
+    [TestMethod]
+    public void GrahamSearch_FourCHPoints_ReturnsCH4()
+    {
+        var a = new Point2(0, 0);
+        var b = new Point2(1, 0);
+        var c = new Point2(1, 1);
+        var d = new Point2(0, 1);
+
+        var ch = ConvexHull.GrahamSearch(new[] { a, b, c, d });
+
+        var points = string.Join(" ", ch);
+        Assert.AreEqual("", points);
     }
 
     public void Sample()
