29249e31692682ee626326d2a1a4b3355a83be20
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-19 22:00:35 +0100

    t Test for triangle point count

--- GeometricaTests/TriangleTests.cs
+++ GeometricaTests/TriangleTests.cs
@@ -31,5 +31,17 @@ namespace GeometricaTests
             Assert.AreEqual(b, t[1]);
             Assert.AreEqual(c, t[2]);
         }
+
+        [TestMethod]
+        public void Triangle_Has_ThreePoints()
+        {
+            var a = new Point2(0, 0);
+            var b = new Point2(1, 0);
+            var c = new Point2(0, 1);
+
+            var t = new Triangle(a, b, c);
+
+            Assert.AreEqual(3, t.Count);
+        }
     }
 }
