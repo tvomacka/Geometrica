033aaf3c5be62dec3fa5ad0766cdcd794bf02ee1
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-19 21:56:09 +0100

    t Test for index point access

--- GeometricaTests/TriangleTests.cs
+++ GeometricaTests/TriangleTests.cs
@@ -17,5 +17,19 @@ namespace GeometricaTests
 
             Assert.AreEqual("Triangle [0; 0] [1; 0] [0; 1]", s);
         }
+
+        [TestMethod]
+        public void TrianglePoints_CanBeAccessed_WithIndex()
+        {
+            var a = new Point2(0, 0);
+            var b = new Point2(1, 0);
+            var c = new Point2(0, 1);
+
+            var t = new Triangle(a, b, c);
+
+            Assert.AreEqual(a, t[0]);
+            Assert.AreEqual(b, t[1]);
+            Assert.AreEqual(c, t[2]);
+        }
     }
 }
