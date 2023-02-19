fe890e758c359d48c0b15bdb2d1a034285abe08b
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-19 21:47:02 +0100

    F!! Triangle implements IPolygon

--- Geometrica/Primitives/Triangle.cs
+++ Geometrica/Primitives/Triangle.cs
@@ -1,5 +1,9 @@
 ﻿namespace Geometrica.Primitives;
 
-public struct Triangle
+public struct Triangle : IPolygon
 {
+    public Point2 this[int key] => throw new NotImplementedException();
+
+    public int Count { get; }
+    public bool IsConvex { get; }
 }
