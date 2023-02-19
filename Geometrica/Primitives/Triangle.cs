61ad91739096f9ed821d42cdc81684c2d083cc67
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-19 21:51:18 +0100

    F!! Triangle can be initialized with three points

--- Geometrica/Primitives/Triangle.cs
+++ Geometrica/Primitives/Triangle.cs
@@ -2,6 +2,17 @@
 
 public struct Triangle : IPolygon
 {
+    private Point2 a;
+    private Point2 b;
+    private Point2 c;
+
+    public Triangle(Point2 a, Point2 b, Point2 c) : this()
+    {
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
     public Point2 this[int key] => throw new NotImplementedException();
 
     public int Count { get; }
--- GeometricaTests/TriangleTests.cs
+++ GeometricaTests/TriangleTests.cs
@@ -1,7 +1,21 @@
-﻿namespace GeometricaTests
+﻿using Geometrica.Primitives;
+
+namespace GeometricaTests
 {
     [TestClass]
     public class TriangleTests
     {
+        [TestMethod]
+        public void Triangle_CanBeInitialized_WithThreePoints()
+        {
+            var a = new Point2(0, 0);
+            var b = new Point2(1, 0);
+            var c = new Point2(0, 1);
+
+            var t = new Triangle(a, b, c);
+            var s = t.ToString();
+
+            Assert.AreEqual("", s);
+        }
     }
 }
