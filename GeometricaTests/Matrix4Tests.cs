05ff229ac1b016e8a2f1211846a8954d4c6a5f94
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-23 21:58:52 +0100

    t initialization test for Matrix4

--- GeometricaTests/Matrix4Tests.cs
+++ GeometricaTests/Matrix4Tests.cs
@@ -1,7 +1,16 @@
-﻿namespace GeometricaTests
+﻿using Geometrica.Algebra;
+
+namespace GeometricaTests
 {
     [TestClass]
     public class Matrix4Tests
     {
+        [TestMethod]
+        public void Matrix4_IsZero_ByDefault()
+        {
+            var m = new Matrix4();
+
+            Assert.AreEqual("", m.ToString());
+        }
     }
 }
