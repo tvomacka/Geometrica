5401ef0ef3bae7db219a9ecd23157c842d51e0e1
Tomáš Vomáčka <t.vomacka@seznam.cz>
2023-02-25 21:47:57 +0100

    F!! DT Initialization, Triangles

--- Geometrica/DataStructures/DelaunayTriangulation.cs
+++ Geometrica/DataStructures/DelaunayTriangulation.cs
@@ -1,5 +1,8 @@
-﻿namespace Geometrica.DataStructures;
+﻿using Geometrica.Primitives;
+
+namespace Geometrica.DataStructures;
 
 public class DelaunayTriangulation
 {
+    public Triangle[] Triangles => Array.Empty<Triangle>();
 }
--- GeometricaTests/DelaunayTriangulationTests.cs
+++ GeometricaTests/DelaunayTriangulationTests.cs
@@ -1,7 +1,16 @@
-﻿namespace GeometricaTests
+﻿using Geometrica.DataStructures;
+
+namespace GeometricaTests
 {
     [TestClass]
     public class DelaunayTriangulationTests
     {
+        [TestMethod]
+        public void DT_HasNotriangles_AfterInitialization()
+        {
+            var dt = new DelaunayTriangulation();
+
+            Assert.AreEqual(0, dt.Triangles.Length);
+        }
     }
 }
